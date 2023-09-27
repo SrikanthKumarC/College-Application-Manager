f7f0027a30c405d9dc3604b12510a997807c93f2
Srikanth C <srikanth@Srikanths-MacBook-Pro.local>
2023-09-27 10:35:27 +0530

    favicon update

---
+++ src/utils/Auth.jsx
@@ -0,0 +1,50 @@
+gimport React, { useContext, useState, useEffect, createContext } from 'react';
+import { supabase } from './Supabase';
+
+// create a context for authentication
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  // create state values for user data and loading
+  const [user, setUser] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // get session data if there is an active session
+    const session = supabase.auth.getSession();
+
+    setUser(session?.user ?? null);
+    setLoading(false);
+
+    // listen for changes to auth
+    const { data: listener } = supabase.auth.onAuthStateChange(
+      (event, session) => {
+        setUser(session?.user ?? null);
+        setLoading(false);
+      }
+    );
+
+    // cleanup the useEffect hook
+   
+  }, []);
+
+  // create signUp, signIn, signOut functions
+  const value = {
+    signUp: data => supabase.auth.signUp(data),
+    signIn: data => supabase.auth.signInWithOAuth(data),
+    signOut: () => supabase.auth.signOut(),
+    user,
+  };
+
+  // use a provider to pass down the value
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};
+
+// export the useAuth hook
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
